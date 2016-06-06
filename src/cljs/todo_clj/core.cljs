fc5055da7f1c85b7761c2233ebe0fa2b6a6e5c25
kprakobkit <kprakobkit@gmail.com>
2016-06-06 14:42:41 -0700

    Deletes todo but doesn't remove it from the list

--- src/cljs/todo_clj/core.cljs
+++ src/cljs/todo_clj/core.cljs
@@ -1,7 +1,7 @@
 (ns todo-clj.core
   (:require [om.core :as om :include-macros true]
             [om.dom :as dom :include-macros true]
-            [ajax.core :refer [GET POST]]))
+            [ajax.core :refer [GET POST DELETE]]))
 
 (enable-console-print!)
 
@@ -13,10 +13,20 @@
 (defn handler [response cursor]
   (om/update! cursor :todos response))
 
-(defn todo [todo]
-  (dom/li #js {:key (:id todo) :className "todo"}
-    (:title todo)
-    (dom/button #js {:className "todo__remove"} "x")))
+
+(defn remove-todo [todo]
+  (let [url (:url todo)]
+    (DELETE url {:handler trace
+                 :error-handler trace
+                 :format :json})))
+
+(defn todo-view [todo]
+  (reify
+    om/IRender
+    (render [_]
+      (dom/li #js {:key (str (:id todo) (rand)) :className "todo"}
+              (:title todo)
+              (dom/button #js {:className "todo__remove" :onClick #(remove-todo todo)} "x")))))
 
 (defn add-todo [app owner]
   (let [title (.-value (om/get-node owner "new-todo"))]    
@@ -48,7 +58,8 @@
                         :placeholder "What needs to be done?"
                         :onKeyPress #(handle-key-press % app owner)}
                    nil)
-        (dom/ul nil (map todo (get app :todos)))))))
+        (apply dom/ul nil
+          (map #(om/build todo-view %) (:todos app)))))))
 
 (om/root
  root-component
