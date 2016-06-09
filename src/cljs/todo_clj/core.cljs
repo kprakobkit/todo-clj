aec65dfbd6dd7502820c2b2e18e8aef70f6a0eba
kprakobkit <kprakobkit@gmail.com>
2016-06-09 14:13:52 -0700

    Shows completed status as checkbox and implements toggling completed status

--- src/cljs/todo_clj/core.cljs
+++ src/cljs/todo_clj/core.cljs
@@ -2,8 +2,8 @@
   (:require-macros [cljs.core.async.macros :refer [go]])
   (:require [om.core :as om :include-macros true]
             [om.dom :as dom :include-macros true]
-            [ajax.core :refer [GET POST DELETE]]
-            [cljs.core.async :refer [put! chan <!]]))
+            [ajax.core :refer [GET POST DELETE PATCH]]
+            [cljs.core.async :refer [put! chan <! alts!]]))
 
 (enable-console-print!)
 
@@ -22,12 +22,23 @@
                  :error-handler trace
                  :format :json})))
 
+(defn update-todo [todo updates handler]
+  (PATCH (:url todo) {:handler handler
+                      :error-handler trace
+                      :params updates
+                      :format :json
+                      :response-format :json
+                      :keywords? true}))
+
 (defn todo-view [todo]
   (reify
     om/IRenderState
-    (render-state [_ {:keys [delete]}]
+    (render-state [_ {:keys [delete update]}]
       (dom/li #js {:key (str (:id todo) (rand)) :className "todo"}
               (:title todo)
+              (dom/input #js {:type "checkbox"
+                              :checked (:completed todo)
+                              :onClick #(update-todo todo {:completed (not (:completed todo))} (fn [updated-todo] (put! update updated-todo)))})
               (dom/button #js {:className "todo__remove"
                                :onClick #(remove-todo todo (fn [_] (put! delete @todo)))} "x")))))
 
@@ -56,20 +67,29 @@
     om/IInitState
     (init-state [_]
       {:delete (chan)
+       :update (chan)
        :title ""})
     om/IWillMount
     (will-mount [_]
-      (let [delete (om/get-state owner :delete)]
+      (let [delete (om/get-state owner :delete)
+            update (om/get-state owner :update)]
         (GET "/todos" {:handler #(handler % app)
                      :keywords? true
                      :response-format :json})
         (go (loop []
-              (let [todo (<! delete)]
-                (om/transact! app :todos
-                   (fn [todos] (vec (remove #(=  todo %) todos))))
+              (let [[todo chan] (alts! [delete update])]
+                (condp = chan
+                  delete
+                  (om/transact! app :todos
+                                (fn [todos] (vec (remove #(=  todo %) todos))))
+                  update
+                  (om/transact! app :todos
+                                (fn [todos]
+                                  (let [idx (.indexOf (map #(:url %) todos) (:url todo))]
+                                    (assoc todos idx todo)))))
                 (recur))))))
     om/IRenderState
-    (render-state [this {:keys [delete title]}]
+    (render-state [this {:keys [delete title update]}]
       (dom/div nil
         (dom/h1 nil "My Todo")
         (dom/input #js {:ref "new-todo"
@@ -83,7 +103,8 @@
            (fn [todo]
              (om/build todo-view todo
                        {:react-key (:id todo)
-                        :init-state {:delete delete}}))
+                        :init-state {:delete delete
+                                     :update update}}))
            (:todos app)))))))
 
 (om/root
