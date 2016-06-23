711145959e4610b8d49f30e7a9152c547f1440da
kprakobkit <kprakobkit@gmail.com>
2016-06-22 22:34:43 -0700

    Move main app component into its own file

---
+++ src/cljs/todo_clj/app.cljs
@@ -0,0 +1,59 @@
+(ns todo-clj.app
+    (:require-macros [cljs.core.async.macros :refer [go]])
+    (:require [om.dom :as dom :include-macros true]
+              [om.core :as om :include-macros true]
+              [cljs.core.async :refer [put! chan <! alts!]]
+              [todo-clj.actions :refer [get-todos add-todo]]
+              [todo-clj.todo :refer [todo-view]]))
+
+(defn handle-key-press [e cursor owner]
+  (let [code (.-charCode e)]
+    (if (= code 13)
+      (add-todo cursor owner))))
+
+(defn handle-change [e owner]
+  (let [title (.. e -target -value)]
+    (om/set-state! owner :title title)))
+
+(defn app [app owner]
+  (reify
+    om/IInitState
+    (init-state [_]
+      {:delete (chan)
+       :update (chan)
+       :title ""})
+    om/IWillMount
+    (will-mount [_]
+      (let [delete (om/get-state owner :delete)
+            update (om/get-state owner :update)]
+        (get-todos app)
+        (go (loop []
+              (let [[todo chan] (alts! [delete update])]
+                (condp = chan
+                  delete
+                  (om/transact! app :todos
+                                (fn [todos] (vec (remove #(=  todo %) todos))))
+                  update
+                  (om/transact! app :todos
+                                (fn [todos]
+                                  (let [idx (.indexOf (map #(:url %) todos) (:url todo))]
+                                    (assoc todos idx todo)))))
+                (recur))))))
+    om/IRenderState
+    (render-state [this {:keys [delete title update]}]
+      (dom/div nil
+        (dom/h1 nil "My Todo")
+        (dom/input #js {:ref "new-todo"
+                        :type "text"
+                        :placeholder "What needs to be done?"
+                        :value title
+                        :onChange #(handle-change % owner)
+                        :onKeyPress #(handle-key-press % app owner)} nil)
+        (apply dom/ul nil
+          (map
+           (fn [todo]
+             (om/build todo-view todo
+                       {:react-key (:id todo)
+                        :init-state {:delete delete
+                                     :update update}}))
+           (:todos app)))))))
