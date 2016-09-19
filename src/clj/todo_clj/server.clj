69bfde1dfa2395d740fe7aa367d3d107c0bd641c
kprakobkit <kprakobkit@gmail.com>
2016-09-19 11:32:29 -0700

    Updates to https

--- src/clj/todo_clj/server.clj
+++ src/clj/todo_clj/server.clj
@@ -55,7 +55,7 @@
     (res-no-content)))
 
 (defn build-host-name [{name :server-name port :server-port}]
-  (str "http://" name ":" port))
+  (str "https://" name ":" port))
 
 (defn expand-url-body [body host-name]
   (prewalk #(if (map? %)
